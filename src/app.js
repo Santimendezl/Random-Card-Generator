a57adfbe0f576026d63e1c22625b559717b32e4d
Santimendezl <santimlacosta@gmail.com>
2023-04-08 13:15:41 +0000

    Version con boton

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cartaGenerator();
+  paloGenerator();
 };
+/*Función genérica Random: retona un valor de un array dado*/
+function randomArray(array) {
+  let rand = array[Math.floor(Math.random() * array.length)];
+  return rand;
+}
+/* Función que genera un número aleatorio*/
+function cartaGenerator() {
+  const numero = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+  document.querySelector("#numero").innerHTML = randomArray(numero);
+}
+/* Función que genera un palo aleatorio*/
+function paloGenerator() {
+  const palo = ["♦", "♥", "♠", "♣"];
+  let paloRandom = randomArray(palo);
+  let paloDiv = document.querySelectorAll(".palo");
+  paloDiv.forEach(element => {
+    if (paloRandom === "♥" || paloRandom === "♦") {
+      element.style.color = "red";
+    } else {
+      element.style.color = "black";
+    }
+    element.innerHTML = paloRandom;
+  });
+}
+
+/* Listener para el botón generador*/
+document.querySelector("#btGenera").addEventListener("click", () => {
+  cartaGenerator();
+  paloGenerator();
+});
